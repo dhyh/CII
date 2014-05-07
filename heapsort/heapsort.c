4e388d21bc2f11dbb0aa65e4ffa62b6b4e9b6207
dongh.life@gmail.com <dongh.life@gmail.com>
2014-05-07 21:02:22 +0800

    新建堆排序目录

---
+++ heapsort/heapsort.c
@@ -0,0 +1,12 @@
+/**
+ * @file heapsort.c
+ * @Synopsis  
+ * @author dh
+ * @version 
+ * @date 2014-05-07
+ */
+
+
+#include "heapsort.h"
+
+
---
+++ heapsort/heapsort.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define PARENT(i) ((i<=1)?(i):(i/2))
+#define RIGHT(i)  (2*i+1)
+#define LEFT(i)   (2*i)
